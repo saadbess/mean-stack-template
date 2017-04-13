efef8373d9fa127e3085d53b3a5bb22f0a1bc614
Saad <saadk@FEEDs-MacBook-Pro-10.local>
2017-04-13 11:43:24 +0100

    added middleware and created routes folder/files

--- app.js
+++ app.js
@@ -5,10 +5,30 @@ const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
 
+// Initialising app variable with express
 const app = express();
 
+// User routes in a seperate file
+const users = require('./routes/users');
+
+// Port variable
 const port = 3000;
 
+// Cors middleware allows to make requests to api from a different domain name
+app.use(cors());
+
+// Body parser middleware
+app.use(bodyParser.json());
+
+// Using the users route
+app.use('/users', users);
+
+// Route to the index homepage sending out some text
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint');
+});
+
+// Calling listen function, takes a port and starts the server
 app.listen(port, () => {
     console.log('Server started on port ' + port);
 });
---
+++ routes/users.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+// Register route
+router.get('/register', (req, res, next) => {
+    res.send('REGISTER');
+});
+
+// Authenticate route
+router.get('/authenticate', (req, res, next) => {
+    res.send('AUTHENTICATE');
+});
+
+// Profile route
+router.get('/profile', (req, res, next) => {
+    res.send('PROFILE');
+});
+
+// Validate route
+router.get('/validate', (req, res, next) => {
+    res.send('VALIDATE');
+});
+
+module.exports = router;
