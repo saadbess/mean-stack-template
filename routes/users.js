54eafbb4dfaa481f7650ce889f631986ccc0ab2f
Saad <saadk@FEEDs-MBP-10.home>
2017-04-14 18:53:36 +0100

    Completed backend API, register route, authentication route/json web token and protected profile route

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const jwt = require('jsonwebtoken');
-
+const config = require('../config/database');
 const User = require('../models/user');
 
 // Register route
@@ -25,12 +25,42 @@ router.post('/register', (req, res, next) => {
 
 // Authenticate route
 router.post('/authenticate', (req, res, next) => {
-    res.send('AUTHENTICATE');
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.getUserByUsername(username, (err, user) => {
+        if(err) throw err;
+        if(!user) {
+            return res.json({success: false, msg: 'User not found'});
+        }
+
+        User.comparePassword(password, user.password, (err, isMatch) => {
+            if(err) throw err;
+            if(isMatch) {
+                const token = jwt.sign(user, config.secret, {
+                    expiresIn: 604800 // 1 week
+                });
+
+                res.json({
+                    success: true,
+                    token: 'JWT ' + token,
+                    user: {
+                        id: user._id,
+                        name: user.name,
+                        username: user.username,
+                        email: user.email
+                    }
+                });
+            } else {
+                return res.json({success: false, msg: 'password is incorrect'});
+            }
+        });
+    });
 });
 
 // Profile route
-router.get('/profile', (req, res, next) => {
-    res.send('PROFILE');
+router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => {
+    res.send({user: req.user});
 });
 
 module.exports = router;
