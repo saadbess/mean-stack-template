7104f969f9cde195d4bf0640fc799b87fba9f780
Saad <saadk@FEEDs-MBP-10.home>
2017-04-14 16:34:16 +0100

    Setup register route

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
 
 // Register route
-router.get('/register', (req, res, next) => {
-    res.send('REGISTER');
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if(err) {
+            res.json({success: false, msg: 'User registration failed'});
+        } else {
+            res.json({success: true, msg: 'Successfully registered user'});
+        }
+    });
 });
 
 // Authenticate route
@@ -14,11 +31,6 @@ router.post('/authenticate', (req, res, next) => {
 // Profile route
 router.get('/profile', (req, res, next) => {
     res.send('PROFILE');
-});
-
-// Validate route
-router.get('/validate', (req, res, next) => {
-    res.send('VALIDATE');
 });
 
 module.exports = router;
